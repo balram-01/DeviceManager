ddd8c038f8c056b4df22e173fb55317f9d9151be
baliram shejal <baliramshejal@balirams-Mac-mini.local>
2024-03-04 20:46:47 +0530

    added location coordinates

--- src/screens/deviceInfo/DeviceInfo.tsx
+++ src/screens/deviceInfo/DeviceInfo.tsx
@@ -1,11 +1,119 @@
-import * as React from 'react';
-
-interface IDeviceInfoProps {
-}
+import * as React from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import colors from "../../theme/colors";
+import Geolocation from "@react-native-community/geolocation";
+import useLocation from "./helper/location";
+interface IDeviceInfoProps {}
 
 const DeviceInfo: React.FunctionComponent<IDeviceInfoProps> = (props) => {
-  return (<>
-  </>);
+  const {
+    locationStatus,
+    requestLocationPermission,
+    currentLongitude,
+    currentLatitude,
+  } = useLocation();
+
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.dataContainer}
+    >
+      <View>
+        <View style={styles.rowContainer}>
+          <Pressable
+            style={styles.btn}
+            onPress={async () => {
+              await requestLocationPermission();
+            }}
+          >
+            <Text style={styles.btnText}>{"Get Current Coordinates"}</Text>
+          </Pressable>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>{`longitude:${
+              currentLongitude ? currentLatitude : 0
+            },latitude:${currentLatitude ? currentLatitude : 0}`}</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <Pressable
+            style={styles.btn}
+            onPress={async () => {
+              await requestLocationPermission();
+            }}
+          >
+            <Text style={styles.btnText}>{"Get Mobile No."}</Text>
+          </Pressable>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>{``}</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <Pressable style={styles.btn} onPress={async () => {}}>
+            <Text style={styles.btnText}>{"Get ImEI No"}</Text>
+          </Pressable>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>{``}</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <Pressable style={styles.btn} onPress={async () => {}}>
+            <Text style={styles.btnText}>{"Get Photo Count"}</Text>
+          </Pressable>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>{``}</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <Pressable style={styles.btn} onPress={async () => {}}>
+            <Text style={styles.btnText}>{"Get Battery %"}</Text>
+          </Pressable>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>{``}</Text>
+          </View>
+        </View>
+        <View style={styles.rowContainer}>
+          <Pressable style={styles.btn} onPress={async () => {}}>
+            <Text style={styles.btnText}>{"Get OS Version"}</Text>
+          </Pressable>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>{``}</Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default DeviceInfo;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  rowContainer: {
+    flexDirection: "row",
+    flex: 1,
+  },
+  btn: {
+    borderColor: colors.primary,
+    borderWidth: 0.5,
+    flex: 1,
+    margin: 10,
+    minHeight: 25,
+  },
+  btnText: {
+    color: colors.primary,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 12,
+    alignSelf: "center",
+
+    flexWrap: "wrap",
+    flex: 1,
+    flexShrink: 1,
+    margin: 6,
+  },
+  dataContainer: {
+    padding: 20,
+  },
+});
---
+++ src/screens/deviceInfo/helper/location.ts
@@ -0,0 +1,118 @@
+// React Native Geolocation
+// https://aboutreact.com/react-native-geolocation/
+
+// import React in our code
+import React, { useState, useEffect } from "react";
+
+// import all the components we are going to use
+import { PermissionsAndroid, Platform } from "react-native";
+
+//import all the components we are going to use.
+import Geolocation from "@react-native-community/geolocation";
+const useLocation = () => {
+  var watchID: any = null;
+  const [currentLongitude, setCurrentLongitude] = useState("");
+  const [currentLatitude, setCurrentLatitude] = useState("");
+  const [locationStatus, setLocationStatus] = useState("");
+
+  const unSubScribeLocation = () => {
+    Geolocation.clearWatch(watchID);
+  };
+  const requestLocationPermission = async () => {
+    if (Platform.OS === "ios") {
+      getOneTimeLocation();
+      subscribeLocationLocation();
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          // @ts-ignore
+          {
+            title: "Location Access Required",
+            message: "This App needs to Access your location",
+          }
+        );
+        console.log("granted",granted)
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          //To Check, If Permission is granted
+          getOneTimeLocation();
+          subscribeLocationLocation();
+        } else {
+          setLocationStatus("Permission Denied");
+        }
+      } catch (err) {
+        console.warn(err);
+      } finally {
+        unSubScribeLocation();
+      }
+    }
+  };
+
+  const getOneTimeLocation = () => {
+    setLocationStatus("Getting Location ...");
+    Geolocation.getCurrentPosition(
+      //Will give you the current location
+      (position: any) => {
+        setLocationStatus("You are Here");
+
+        //getting the Longitude from the location json
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Longitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      (error: any) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 1000,
+      }
+    );
+  };
+
+  const subscribeLocationLocation = () => {
+    watchID = Geolocation.watchPosition(
+      (position: any) => {
+        //Will give you the location on location change
+
+        setLocationStatus("You are Here");
+        console.log(position);
+
+        //getting the Longitude from the location json
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+
+        //getting the Latitude from the location json
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Latitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      (error: any) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 1000,
+      }
+    );
+  };
+  return {
+    currentLatitude,
+    currentLongitude,
+    locationStatus,
+    requestLocationPermission,
+  };
+};
+
+export default useLocation;
